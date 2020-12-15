20354581755e5ba6c9dbd86ea4d33b00cbd810a9
Ron Marinusse <ron.marinusse@agfa.com>
2020-12-15 15:12:44 +0100

    Day 13 Pt2

--- Day13/Program.cs
+++ Day13/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Numerics;
 
 namespace Day13
 {
@@ -6,7 +8,7 @@ namespace Day13
     {
         private static int timeStamp = 1006697;
 
-        private static string[] schedule =
+        private static string[] input =
             "13,x,x,41,x,x,x,x,x,x,x,x,x,641,x,x,x,x,x,x,x,x,x,x,x,19,x,x,x,x,17,x,x,x,x,x,x,x,x,x,x,x,29,x,661,x,x,x,x,x,37,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,23"
                 .Split(',', StringSplitOptions.RemoveEmptyEntries);
 
@@ -20,19 +22,39 @@ namespace Day13
         {
             var waitTime = int.MaxValue;
             var busId = 0;
-            foreach (var s in schedule)
+            foreach (var s in input)
             {
                 if (!int.TryParse(s, out var id)) continue;
                 var remainder = timeStamp % id;
-                if (id-remainder >= waitTime) continue;
-                waitTime = id-remainder;
+                if (id - remainder >= waitTime) continue;
+                waitTime = id - remainder;
                 busId = id;
             }
-            Console.WriteLine($"busId: {busId}, waitTime: {waitTime}. Answer {busId*waitTime}");
+
+            Console.WriteLine($"busId: {busId}, waitTime: {waitTime}. Answer {busId * waitTime}");
         }
 
         static void Two()
         {
+            ulong step = 1;
+            ulong ts = 0;
+            //var schedule = input.Select(x => x == "x" ? 1 : int.Parse(x)).ToList();
+            var schedule = input.Select((x, i) => (x, i)).Where(x => x.x != "x")
+                .Select(b => new {id = ulong.Parse(b.x), offset = (ulong)b.i}).ToList();
+
+            while (schedule.Count > 0)
+            {
+                ts += step;
+                if ((ts + schedule[0].offset) % schedule[0].id == 0)
+                {
+                    step *= schedule[0].id;
+                    schedule.RemoveAt(0);
+                }
+
+                Console.Write($"\rTimestamp {ts}");
+            }
+
+            Console.WriteLine($"Timestamp {ts}");
         }
     }
 }
