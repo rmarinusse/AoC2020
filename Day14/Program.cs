40820d00c84024ed754dc2d292fdf15695789cb3
Ron Marinusse <ron.marinusse@agfa.com>
2020-12-16 20:59:25 +0100

    Day14 Pt2

--- Day14/Program.cs
+++ Day14/Program.cs
@@ -11,11 +11,13 @@ namespace Day14
     class Program
     {
         private static string[] input;
+
         static void Main(string[] args)
         {
             var sr = new StreamReader(@"input.txt");
             input = sr.ReadToEnd().Split("\r\n", StringSplitOptions.RemoveEmptyEntries);
             One();
+            Two();
         }
 
         static void One()
@@ -58,7 +60,61 @@ namespace Day14
 
         static void Two()
         {
+            var mem = new Dictionary<long, long>();
+            var mask = "";
+            for (var i = 0; i < input.Length; i++)
+            {
+                var parts = input[i].Split('=', StringSplitOptions.RemoveEmptyEntries);
+                var op = parts[0].Trim();
+                var val = parts[1].Trim();
+                if (op.Equals("mask"))
+                {
+                    mask = val;
+                }
+                else if (op.StartsWith("mem"))
+                {
+                    var addresses = new List<string> {Convert.ToString(int.Parse(op.Substring(4).Trim(']')), 2).PadLeft(36,'0')};
+                    var dec = int.Parse(val);
+                    for (var j = 0; j < mask.Length; j++)
+                    {
+                        switch (mask[j])
+                        {
+                            case '1':
+                                for (var k = 0; k < addresses.Count; k++)
+                                {
+                                    var sb = new StringBuilder(addresses[k]) {[j] = '1'};
+                                    addresses[k] = sb.ToString();
+                                }
 
+                                break;
+                            case 'X':
+                                var newAddresses = new List<string>();
+                                for (var k = 0; k < addresses.Count; k++)
+                                {
+                                    var sb1 = new StringBuilder(addresses[k]) {[j] = '0'};
+                                    newAddresses.Add(sb1.ToString());
+                                    var sb2 = new StringBuilder(addresses[k]) {[j] = '1'};
+                                    newAddresses.Add(sb2.ToString());
+                                }
+
+                                addresses = newAddresses;
+                                break;
+                        }
+                    }
+
+                    addresses.ForEach(a =>
+                    {
+                        var address=Convert.ToInt64(a,2);
+                        if (mem.ContainsKey(address))
+                            mem[address] = dec;
+                        else
+                            mem.Add(address, dec);
+                    });
+                }
+            }
+
+            Console.WriteLine($"Sum of values in memory {mem.Values.Sum()}");
         }
     }
 }
+
